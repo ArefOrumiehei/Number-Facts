0e33bf3b0d89ccf29b5183dd726c09cd41d9bf6b
Aref Orumiehei <areforumiehei@gmail.com>
2023-01-28 23:48:50 +0330

    add all of files

---
+++ app.js
@@ -0,0 +1,39 @@
+// access to form
+const form = document.querySelector('#form')
+
+form.addEventListener('submit' , numberFact)
+
+
+
+function numberFact(e){
+    e.preventDefault()
+    
+    const numberInput = document.querySelector('#number').value
+    const numberType = document.querySelector('#number-type').value
+    const result = document.querySelector('#result')
+    let url = 'http://numbersapi.com/'
+
+    // make URL (number)
+    if (numberInput !== '') {
+        url = url + (numberInput + '/')
+    } else {
+        url = url + 'random/'
+    }
+
+    // make url (type of number)
+    if (numberType !== '') {
+        url += (numberType + '/')
+    }
+console.log(url);
+    // Ajax
+    const xhr = new XMLHttpRequest()
+
+    xhr.open("GET" , url , true)
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            result.innerHTML = `${this.responseText}`
+        }
+    }
+    xhr.send()
+}
